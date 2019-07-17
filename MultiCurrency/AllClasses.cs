6acc386be55176e45a5106e3cb9a6b947de0d1ff
Dave Gines <dgines@rentdynamics.com>
2019-07-17 11:50:08 -0400

    Project created

---
+++ MultiCurrency/AllClasses.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MultiCurrency
+{
+  public class Dollar
+  {
+  }
+}
---
+++ UnitTests/MultiCurrencyTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+  public class MultiCurrencyTests
+  {
+    [Fact]
+    public void Test1()
+    {
+    }
+  }
+}
